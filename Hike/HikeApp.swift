df587bfffb90dd7cfd29825e82a48dfda7f951d3
Junichi-Machii <junichi878338@hotmail.co.jp>
2023-08-29 15:52:50 +0900

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Junichi Machii on 2023/08/29.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
