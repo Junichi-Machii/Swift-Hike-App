4cf86310b3c0382bdc039ac13e28c6bf3f275612
Junichi-Machii <junichi878338@hotmail.co.jp>
2023-09-13 17:05:01 +0900

    add card button

--- Hike/Conponent/CardView.swift
+++ Hike/Conponent/CardView.swift
@@ -9,23 +9,53 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
+        
         ZStack {
-            Circle()
-                .fill(
-                 LinearGradient(
-                    colors:[
-                      Color("ColorIndigoMedium"),
-                      Color("ColorSalmonLight")
-                    ],
-                    startPoint: .topLeading,
-                    endPoint: .bottomTrailing
-                 )
-                )
-                .frame(width: 256,height: 256)
-            Image("image-1")
-                .resizable()
-            .scaledToFit()
-        }
+            CustomBackgroundView()
+            VStack {
+                VStack(alignment:.listRowSeparatorLeading){ HStack {
+                    Text("Hiking")
+                        .fontWeight(.black)
+                        .font(.system(size: 52))
+                        .foregroundStyle(
+                            LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+                    
+                    Spacer()
+                    
+                    Button {
+                        // Action: Show a Sheet
+                        print("The  button was pressed.")
+                    }label: {
+                        CustomButtonIView()
+                        
+                    }
+                }
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }
+                .padding(.horizontal,40)
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors:[
+                                    Color("ColorIndigoMedium"),
+                                    Color("ColorSalmonLight")
+                                ],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .frame(width: 256,height: 256)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+        }// CARD
+        .frame(width: 320,height: 570)
     }
 }
 
